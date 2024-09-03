3d96cce3be8f85a9a598d0ca70191aba9d990b85
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-09-03 20:38:10 +0900

    Navbar,Navitem 생성

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Adminpage = () => {
+  return <div>관리자 전용</div>;
+};
+
+export default Adminpage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <div>로그인 유저만 확인할 수 있는 페이지</div>;
+};
+
+export default UserPage;
